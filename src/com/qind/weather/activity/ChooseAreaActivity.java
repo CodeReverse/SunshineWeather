dfc863244ca03c51c70d705ff8db0b2ab45cf0ac
Reversal <18918486029@163.com>
2015-04-17 10:03:56 +0800

    新增网络请求，数据操作类

---
+++ src/com/qind/weather/activity/ChooseAreaActivity.java
@@ -0,0 +1,14 @@
+package com.qind.weather.activity;
+
+import com.qind.weather.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChooseAreaActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.choose_area);
+	}
+}
---
+++ src/com/qind/weather/utils/HttpUtil.java
@@ -0,0 +1,57 @@
+package com.qind.weather.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtil {
+	/*
+	 * 回调接口
+	 */
+	public interface HttpCallbackListener {
+		void onFinish(String response);
+
+		void onFailed(Exception e);
+	}
+
+	public static void sendHttpRequest(final String address,
+			final HttpCallbackListener listener) {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				HttpURLConnection urlConnection = null;
+				try {
+					URL url = new URL(address);
+					urlConnection = (HttpURLConnection) url.openConnection();
+					urlConnection.setRequestMethod("GET");
+					urlConnection.setConnectTimeout(5000);
+					urlConnection.setReadTimeout(5000);
+					InputStream in = urlConnection.getInputStream();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(in));
+					StringBuilder response = new StringBuilder();
+					String line;
+					while ((line = reader.readLine()) != null) {
+						response.append(line);
+					}
+					if (listener != null) {
+						listener.onFinish(response.toString());
+					}
+				} catch (Exception e) {
+					// TODO: handle exception
+					if (listener != null) {
+						listener.onFailed(e);
+					}
+				} finally {
+					if (urlConnection != null) {
+						urlConnection.disconnect();
+					}
+				}
+			}
+		}).start();
+	}
+}
